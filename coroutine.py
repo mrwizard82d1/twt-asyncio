668d345b7055e99cafde33f351aa3cbd48569086
Larry Jones <mrwizard82d1@gmail.com>
2025-03-10 13:55:37 -0500

    Use TaskGroup to improve handling of multiple asyncio tasks

--- coroutine.py
+++ coroutine.py
@@ -9,17 +9,19 @@ async def fetch_data(task_id, sleep_time):
 
 
 
-# Illustrate the `asyncio.gather()` function
+# Use `asyncio.TaskGroup` for running multiple, parallel tasks
 async def main():
-    # Run coroutines in parallel gathering the returned values from
-    # multiple coroutines into `results`.
-    results = await asyncio.gather(
-        fetch_data(1, 2), fetch_data(2, 1), fetch_data(3, 3)
-    )
+    tasks = []
+    async with asyncio.TaskGroup() as tg:
+        for i, sleep_time in enumerate([2, 1, 3], start=1):
+            task = tg.create_task(fetch_data(i, sleep_time))
+            tasks.append(task)
 
-    # Process the gathered results
+    # At this point in execution, all tasks have completed
+    results = [task.result() for task in tasks]
+
     for result in results:
-        print(result)
+        print(f'Received {result=}')
 
 
 asyncio.run(main())

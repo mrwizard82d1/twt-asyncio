d3745e73c09aa500a8bc102fa92f16d786882507
Larry Jones <mrwizard82d1@gmail.com>
2025-03-10 11:18:16 -0500

    One coroutine awaiting completion of another coroutine

--- coroutine.py
+++ coroutine.py
@@ -1,12 +1,34 @@
 import asyncio
 
 
+# Define a coroutine that simulates a time-consuming task
+async def fetch_data(delay):
+    print('Fetching data...')
+
+    # Simulate an I/O operation with a call to `sleep()`
+    await asyncio.sleep(delay)
+
+    print('Data fetched')
+
+    return {'data': 'Some data'}
+
+
+# Define a coroutine that calls the first coroutine.
 async def main():
     """
-    Returns a coroutine that simply prints out a message.
+    Returns a coroutine that awaits data from another coroutine.
     :return: None
     """
     print('Start of main coroutine')
+    task = fetch_data(2)
+
+    # Await completion of the `fetch_data()` coroutine.
+    # This action pauses the `main()` coroutine until
+    # the `fetch_data()` call completes.
+    result = await task
+
+    print(f'Received result: {result}')
+    print('End of main coroutine')
 
 
 asyncio.run(main())

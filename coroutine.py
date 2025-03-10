027dc62fafb55fa15e716fd9b2a2252485063c03
Larry Jones <mrwizard82d1@gmail.com>
2025-03-10 11:28:26 -0500

    Await two tasks **not** running in parallel

--- coroutine.py
+++ coroutine.py
@@ -2,15 +2,15 @@ import asyncio
 
 
 # Define a coroutine that simulates a time-consuming task
-async def fetch_data(delay):
-    print('Fetching data...')
+async def fetch_data(delay, id):
+    print(f'Fetching data... {id=}')
 
     # Simulate an I/O operation with a call to `sleep()`
     await asyncio.sleep(delay)
 
-    print('Data fetched')
+    print(f'Data fetched. {id=}')
 
-    return {'data': 'Some data'}
+    return {'data': 'Some data', 'id': id}
 
 
 # Define a coroutine that calls the first coroutine.
@@ -19,15 +19,19 @@ async def main():
     Returns a coroutine that awaits data from another coroutine.
     :return: None
     """
-    print('Start of main coroutine')
-    task = fetch_data(2)
-    print('End of main coroutine')
+    task1 = fetch_data(2, 1)
+    task2 = fetch_data(2, 2)
 
-    # Await completion of the `fetch_data()` coroutine.
-    # This action pauses the `main()` coroutine until
-    # the `fetch_data()` call completes.
-    result = await task
-    print(f'Received result: {result}')
+    # At first glance, we might think than `task1` and `task2` are
+    # running in-parallel; however, because we are awaiting each task,
+    # they **do not** run in parallel but **sequentially**. That is,
+    # running this code takes **4** seconds instead of about 2 seconds.
+
+    result1 = await task1
+    print(f'Received result: {result1}')
+
+    result2 = await task2
+    print(f'Received result: {result2}')
 
 
 asyncio.run(main())

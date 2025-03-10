49d3416361f52ca0009018ef2782c33dba008581
Larry Jones <mrwizard82d1@gmail.com>
2025-03-10 11:47:17 -0500

    Run two tasks concurrently followed by a third

--- coroutine.py
+++ coroutine.py
@@ -9,18 +9,15 @@ async def fetch_data(task_id, sleep_time):
 
 
 async def main():
-    # Create `asyncio` **tasks** for multiple coroutines that we may run concurrently
+    # Run two tasks concurrently
     task1 = asyncio.create_task(fetch_data(1, 2))
     task2 = asyncio.create_task(fetch_data(2, 3))
-    task3 = asyncio.create_task(fetch_data(3, 1))
 
-    # Because we invoked `asyncio.create_task()` for each chunk of
-    # work, awaiting these three tasks runs these tasks **in parallel**.
-    # As a consequence, instead of taking about 6 seconds to complete,
-    # these three tasks complete in about 3 seconds (the time of the
-    # longest task).
     result1 = await task1
     result2 = await task2
+
+    # Followed by the third task (taking about 4 seconds to complete)
+    task3 = asyncio.create_task(fetch_data(3, 1))
     result3 = await task3
 
     print(result1, result2, result3)

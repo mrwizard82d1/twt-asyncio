5c7bc901483dd8a362b4c385489a407b7c81f893
Larry Jones <mrwizard82d1@gmail.com>
2025-03-10 11:55:42 -0500

    Using asyncio.gather() for multiple tasks

--- coroutine.py
+++ coroutine.py
@@ -8,19 +8,18 @@ async def fetch_data(task_id, sleep_time):
     return {'id': task_id, 'data': f'Sample data from coroutine {task_id}'}
 
 
+
+# Illustrate the `asyncio.gather()` function
 async def main():
-    # Run two tasks concurrently
-    task1 = asyncio.create_task(fetch_data(1, 2))
-    task2 = asyncio.create_task(fetch_data(2, 3))
+    # Run coroutines in parallel gathering the returned values from
+    # multiple coroutines into `results`.
+    results = await asyncio.gather(
+        fetch_data(1, 2), fetch_data(2, 1), fetch_data(3, 3)
+    )
 
-    result1 = await task1
-    result2 = await task2
-
-    # Followed by the third task (taking about 4 seconds to complete)
-    task3 = asyncio.create_task(fetch_data(3, 1))
-    result3 = await task3
-
-    print(result1, result2, result3)
+    # Process the gathered results
+    for result in results:
+        print(result)
 
 
 asyncio.run(main())

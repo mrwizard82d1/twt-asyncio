026c75607a6f29196495d09101da2a86ad400862
Larry Jones <mrwizard82d1@gmail.com>
2025-03-10 11:45:27 -0500

    Finally, await three tasks running **concurrently**

--- coroutine.py
+++ coroutine.py
@@ -2,36 +2,28 @@ import asyncio
 
 
 # Define a coroutine that simulates a time-consuming task
-async def fetch_data(delay, id):
-    print(f'Fetching data... {id=}')
-
-    # Simulate an I/O operation with a call to `sleep()`
-    await asyncio.sleep(delay)
-
-    print(f'Data fetched. {id=}')
-
-    return {'data': 'Some data', 'id': id}
+async def fetch_data(task_id, sleep_time):
+    print(f'Coroutine {task_id} starting to fetch data.')
+    await asyncio.sleep(sleep_time)
+    return {'id': task_id, 'data': f'Sample data from coroutine {task_id}'}
 
 
-# Define a coroutine that calls the first coroutine.
 async def main():
-    """
-    Returns a coroutine that awaits data from another coroutine.
-    :return: None
-    """
-    task1 = fetch_data(2, 1)
-    task2 = fetch_data(2, 2)
+    # Create `asyncio` **tasks** for multiple coroutines that we may run concurrently
+    task1 = asyncio.create_task(fetch_data(1, 2))
+    task2 = asyncio.create_task(fetch_data(2, 3))
+    task3 = asyncio.create_task(fetch_data(3, 1))
 
-    # At first glance, we might think than `task1` and `task2` are
-    # running in-parallel; however, because we are awaiting each task,
-    # they **do not** run in parallel but **sequentially**. That is,
-    # running this code takes **4** seconds instead of about 2 seconds.
-
+    # Because we invoked `asyncio.create_task()` for each chunk of
+    # work, awaiting these three tasks runs these tasks **in parallel**.
+    # As a consequence, instead of taking about 6 seconds to complete,
+    # these three tasks complete in about 3 seconds (the time of the
+    # longest task).
     result1 = await task1
-    print(f'Received result: {result1}')
-
     result2 = await task2
-    print(f'Received result: {result2}')
+    result3 = await task3
+
+    print(result1, result2, result3)
 
 
 asyncio.run(main())

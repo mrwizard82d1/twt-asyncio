4d9591956becda16cc0d82b676c63feb84ec9e0b
Larry Jones <mrwizard82d1@gmail.com>
2025-03-10 14:34:43 -0500

    Two tasks in parallel; third later

--- basic_tasks.py
+++ basic_tasks.py
@@ -12,10 +12,11 @@ async def main():
     # Create tasks for running coroutines concurrently
     task1 = asyncio.create_task(fetch_data(1, 2))
     task2 = asyncio.create_task(fetch_data(2, 3))
-    task3 = asyncio.create_task(fetch_data(3, 1))
 
     result1 = await task1
     result2 = await task2
+
+    task3 = asyncio.create_task(fetch_data(3, 1))
     result3 = await task3
 
     print(result1, result2, result3)
